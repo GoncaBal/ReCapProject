67fddfc58df5df8b15a7b4eda2a1e117d69ea705
GoncaBal <goncabal94@gmail.com>
2021-02-21 01:05:45 +0300

    .

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -14,7 +14,7 @@ namespace ConsoleUI
             carManager.Add(new Entities.Concrete.Car { CarId=3,BrandId=1,ColorId=3,ModelYear=2020,DailyPrice=800,Description="BMW M5"});
             foreach (var car in carManager.GetAll())
             {
-                Console.WriteLine("{0}: {1} Model {2} Günlük Ücret:{3}TL",car.CarId,car.ModelYear,car.Description,car.DailyPrice);
+                Console.WriteLine("{0}: {1} Model {2} Günlük Ücret:{3} TL",car.CarId,car.ModelYear,car.Description,car.DailyPrice);
             }
         }
     }
